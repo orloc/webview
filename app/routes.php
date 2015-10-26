64dbea3468e4a6a780aff7c3396e0c6d923ebecc
Grant Tepper <grant.tepper@gmail.com>
2015-10-26 02:17:42 -0400

    stubs out controller so submit works - adds guzzle - starts definign search api

--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,14 @@
 <?php
 
+use \Symfony\Component\HttpFoundation\Request;
+
 $app->get('/', function() use ($app){
     return $app['twig']->render('page.html.twig');
+});
+
+$app->post('/search', function(Request $request) use ($app){
+    var_dump($request->request->all());
+    die;
+
 });
 
--- web/js/controller/mainController.js
+++ web/js/controller/mainController.js
@@ -1,5 +1,20 @@
 angular.module('webview')
 .controller('mainController', [ '$scope', '$http', function($scope, $http){
+        $scope.input = {};
+
+        var success = function(data){
+
+            console.log(data);
+
+        };
+
+        var error = function(error){
+
+        };
+
+        $scope.submit = function(){
+            $http.post('/search', $scope.input).then(success, error);
+        };
 
 
     }]);
